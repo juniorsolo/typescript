bc1563852e6a4daa7b719078293bfbe034169754
juniorsolo <junior.solo@hotmail.com>
2018-11-05 20:31:50 -0200

    Primeira aula de Type Script.

---
+++ inference.ts
@@ -0,0 +1,18 @@
+function showLog(message: any){
+    console.log(message);
+}
+
+showLog("Hello Word!");
+showLog(139);
+/**
+ * string
+ * number
+ * boolean
+ * arrays
+ * Classe
+ */
+
+ let test: Array<string> = ['Hello','Test'];
+ let isEnabled: boolean = true;
+ let test2: [string] = ['xcs'];
+ showLog(isEnabled);
