02636ca6271db6e1edc9f2d34ff0c6bca29fe5e7
juniorsolo <junior.solo@hotmail.com>
2018-11-20 12:57:51 -0200

    Exemplo com ngfor.

--- angular/first-angular-cli/src/app/app.component.ts
+++ angular/first-angular-cli/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = ' hello word angular CLI';
+  title = 'Lista de Tarefas';
+  tasks = [];
+  task="";
+
+  add() :void{
+    this.tasks.push(this.task);
+    this.task = "";
+  }
 }
